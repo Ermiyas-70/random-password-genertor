1ea3eb6d657a511e391917a79c375565422768ab
Ermiyas-70 <ermiyastomas70@gmail.com>
2026-07-15 00:08:12 +0300

    Added index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const btnEl = document.querySelector(".btn");
+
+
+
+btnEl.addEventListener("click", () => {
+    createPassword();
+});
+
+function createPassword() {
+    const chars =
+        "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = 14;
+    let password = "";
+    for (let index = 0; index < passwordLength; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNum, randomNum + 1);
+    }
+}
